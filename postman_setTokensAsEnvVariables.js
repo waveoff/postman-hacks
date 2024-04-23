1a9e8e169df256359543de6361d8fe10eec674b8
The Great Wave off Ramen <3776596+waveoff@users.noreply.github.com>
2024-04-23 17:01:25 +0200

    add: postman_setTokensAsEnvVariables.js

---
+++ postman_setTokensAsEnvVariables.js
@@ -0,0 +1,19 @@
+// POSTMAN: API AUTH BEARER UPDATE AUTOMATION
+
+// I'm using this code to make it easy to refresh the environment variables 
+// used as storage for API authorization bearers in Postman. 
+
+// 1. Create Postman Env Variables to store your Access and Refresh Tokens
+// 2. Create an Auth Request
+// 3. Put this code inside the Test tab of the Auth Request
+// 4. Replace "YOUR_RESPONSE_JSON_..._TOKEN_KEY" placeholders with your real JSON keys (fron API auth response)
+// 5. Send Auth Request, check it went successfuly and you have proper Auth Response (Status: 200 OK)
+// 6. Check if your Env Variables are filled with values from Auth Response JSON
+// 7. Enjoy your automation :)
+
+pm.test("Set new EnvVar token", function () {
+    let authToken = pm.response.json().YOUR_RESPONSE_JSON_AUTH_TOKEN_KEY;
+    let refreshToken = pm.response.json().YOUR_RESPONSE_JSON_REFRESH_TOKEN_KEY;
+    pm.environment.set("YOUR_ACCESS_TOKEN_ENV_VARIABLE_NAME", authToken);
+    pm.environment.set("YOUR_REFRESH_TOKEN_ENV_VARIABLE_NAME", refreshToken);
+});
