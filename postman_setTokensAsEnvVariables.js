9650eab05457e8a6f969a018046b69768d0d435d
The Great Wave off Ramen <whoisariross@gmail.com>
2024-04-24 10:54:18 +0200

    made it shorter and cleaner

--- postman_setTokensAsEnvVariables.js
+++ postman_setTokensAsEnvVariables.js
@@ -10,9 +10,7 @@
 // 6. Check if your Env Variables are filled with values from the Auth Response JSON
 // 7. Enjoy your automation :)
 
-pm.test("Set new EnvVar token", function () {
-    let authToken = pm.response.json().YOUR_RESPONSE_JSON_AUTH_TOKEN_KEY;
-    let refreshToken = pm.response.json().YOUR_RESPONSE_JSON_REFRESH_TOKEN_KEY;
-    pm.environment.set("YOUR_ACCESS_TOKEN_ENV_VARIABLE_NAME", authToken);
-    pm.environment.set("YOUR_REFRESH_TOKEN_ENV_VARIABLE_NAME", refreshToken);
+pm.test("Update auth token (EnvVar)", function () {
+    pm.environment.set("YOUR_ACCESS_TOKEN_ENV_VARIABLE_NAME", pm.response.json().YOUR_RESPONSE_JSON_AUTH_TOKEN_KEY);
+    pm.environment.set("YOUR_REFRESH_TOKEN_ENV_VARIABLE_NAME", pm.response.json().YOUR_RESPONSE_JSON_REFRESH_TOKEN_KEY);
 });
